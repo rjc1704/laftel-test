be1ae18d5a98781dce45f608ba3f9bde8edef63a
Jecheon Ryu <rjc1704@gmail.com>
2023-12-28 14:11:31 +0900

    node-fetch 적용2

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get("/laftel", async (req, res) => {
-  //   const result = await axios
-  //     .get("https://laftel.net/api/items/v1/hot/")
-  //     .then((res) => res.data);
-  const result = await fetch(
-    "https://jsonplaceholder.typicode.com/todos/1"
-  ).then((response) => response.json());
+  const result = await fetch("https://laftel.net/api/items/v1/hot/").then(
+    (res) => res.json()
+  );
   res.json(result);
 });
 
