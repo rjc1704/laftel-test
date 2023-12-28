7f0161087800647ff6dd8d372a9ee71a5f174a17
Jecheon Ryu <rjc1704@gmail.com>
2023-12-28 14:46:38 +0900

    node-fetch 적용3

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const app = express();
 const port = 3000;
 
 app.get("/laftel", async (req, res) => {
-  const result = await fetch("https://laftel.net/api/items/v1/hot/").then(
-    (res) => res.json()
-  );
+  const result = await fetch("https://laftel.net/api/items/v1/hot/", {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    },
+  }).then((res) => res.json());
   res.json(result);
 });
 
